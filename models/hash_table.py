2b050373594d06cff76f65bb4806aa2f79b224e7
SamuelBottermundFlores <sbflores55344@gmail.com>
2026-05-23 17:11:49 -0300

    config hash

--- models/hash_table.py
+++ models/hash_table.py
@@ -1,26 +1,45 @@
-import hashlib
-
 class HashTable:
     def __init__(self):
-        self.table = {}
-    
+        self.size = 10
+        self.count = 0
+        self.table = [[] for _ in range(self.size)]
+
     def gera_hash(self, recipe):
-        data = ""
-        data += recipe.name
+        data = recipe.name
         data += "".join(recipe.ingredients)
         data += "".join(recipe.instructions)
         data += str(recipe.prepTimeMinutes)
         data += str(recipe.cookTimeMinutes)
+        hash_value = 0
+        for char in data:
+            hash_value += ord(char)
+        return hash_value
 
-        key_hash = hashlib.sha256(data.encode()).hexdigest()
-        return key_hash
-    
+    def get_index(self, recipe):
+
+        return self.gera_hash(recipe) % self.size
+
+    def resize(self):
+        old_table = self.table
+        self.size *= 2
+        self.table = [[] for _ in range(self.size)]
+        self.count = 0
+        for bucket in old_table:
+            for saved_hash, recipe in bucket:
+                self.insert(recipe)
+
     def insert(self, recipe):
-        key_hash = self.gera_hash(recipe)
-        self.table[recipe.id] = key_hash
+        if self.count / self.size >= 1.0:
+            self.resize()
+        index = self.get_index(recipe)
+        recipe_hash = self.gera_hash(recipe)
+        self.table[index].append((recipe_hash, recipe))
+        self.count += 1
 
     def verify(self, recipe):
-        new_hash = self.gera_hash(recipe)
-        old_hash = self.table.get(recipe.id)
-
-        return new_hash == old_hash
+        index = self.get_index(recipe)
+        current_hash = self.gera_hash(recipe)
+        for saved_hash, saved_recipe in self.table[index]:
+            if saved_recipe.id == recipe.id:
+                return saved_hash == current_hash
+        return False
